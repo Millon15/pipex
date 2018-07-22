d251685198d1f9d51255b69a67289c18bcfc7c0b
vbrazas <appleitunesstore11@gmail.com>
2018-07-22 17:05:32 +0300

    Done all

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vbrazas <vbrazas@student.unit.ua>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 15:14:41 by vbrazas           #+#    #+#             */
-/*   Updated: 2018/07/21 21:36:46 by vbrazas          ###   ########.fr       */
+/*   Updated: 2018/07/22 17:03:11 by vbrazas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,62 +24,57 @@ int				put_usage(const int errnum)
 	return (1);
 }
 
-char			*pipex_join(char *str1, char *str2)
+void			forkwork(const int to_stdin, const int to_stdout,\
+	char *const comm[], char *const env[])
 {
-	char *tmp2;
-
-	tmp2 = str1;
-	str1 = ft_strjoin(str1, str2);
-	free(tmp2);
-	return (str1);
-}
-
-char			*pars(int ac, char **av)
-{
-	int		i;
-	char	*str;
-
-	i = 0;
-	while (++i < ac)
-	{
-		if (i == 1)
-			str = ft_strjoin("< ", av[i]);
-		else if (i == ac - 1)
-		{
-			str = pipex_join(str, " > ");
-			str = pipex_join(str, av[i]);
-		}
-		else
-		{
-			str = pipex_join(str, " ");
-			str = pipex_join(str, av[i]);
-			if (i != ac - 2)
-				str = pipex_join(str, " |");
-		}
-	}
-	return (str);
-}
-
-void			start(int ac, char **av)
-{
-	char *const		c[4] = {"/bin/zsh", "-c", pars(ac, av), NULL};
-	char *const		e[1] = {NULL};
 	pid_t			pid;
 
 	((pid = fork()) == -1) ? put_usage(0) : false;
 	if (pid == 0)
 	{
-		execve("/bin/zsh", c, e);
-		put_usage(0);
+		dup2(to_stdout, 1);
+		close(to_stdout);
+		dup2(to_stdin, 0);
+		close(to_stdin);
+		execve(comm[0], comm, env);
+		_exit(0);
 	}
-	free(c[2]);
 }
 
-int				main(int ac, char **av)
+void			start(int ac, char **av, char *const comm[], char *const env[])
 {
+	const int		file1 = open(av[1], O_RDONLY);
+	const int		file2 = open(av[ac], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	int				pipex[2];
+	int				i;
+
+	(file1 == -1 || file2 == -1) ? put_usage(0) : false;
+	((pipe(pipex)) == -1) ? put_usage(0) : false;
+	i = 1;
+	while (++i < ac)
+	{
+		((char **)comm)[2] = av[i];
+		forkwork(file1, pipex[1], comm, env);
+		((char **)comm)[2] = av[i];
+		forkwork(pipex[0], file2, comm, env);
+	}
+	close(file1);
+	close(file2);
+	close(pipex[0]);
+	close(pipex[1]);
+}
+
+int				main(int ac, char **av, char **env)
+{
+	char *const		comm[4] = {"/bin/zsh", "-c", "", NULL};
+
 	if (ac < 5)
+	{
 		put_usage(1);
+	}
 	else
-		start(ac, av);
+	{
+		start(ac - 1, av, comm, env);
+	}
 	return (0);
 }
